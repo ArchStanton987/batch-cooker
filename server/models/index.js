159103849f7dbaa4175c445f4b92e2d273647168
Pierre Moulin <phv.moulin@gmail.com>
2020-09-16 13:52:00 +0200

    reorganized app

--- server/models/index.js
+++ server/models/index.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize')
 const basename = path.basename(__filename)
 require('dotenv').config({ path: path.resolve('../', '../', '.env') })
 const env = process.env.NODE_ENV || 'development'
-const config = require('../config/sequelize-config.js')[env]
+const config = require('../../config/sequelize-config.js')[env]
 const db = {}
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config)
---
+++ server/server.js
@@ -0,0 +1,103 @@
+require('dotenv').config()
+const path = require('path')
+require('dotenv').config({ path: path.resolve('../', '.env') })
+const express = require('express')
+const cors = require('cors')
+const connection = require('./conf.js')
+const bodyParser = require('body-parser')
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
+const bcrypt = require('bcrypt')
+
+const users = require('./routes/users')
+
+const models = require('./db/models')
+
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+app.use(cookieParser())
+
+app.use('/api/users', users)
+
+app.listen(process.env.SERVER_PORT, err => {
+  if (err) {
+    throw new Error('Something bad happened...')
+  }
+  console.log(`Server is listening on ${process.env.SERVER_PORT}`)
+})
+
+function verifyToken(req, res, next) {
+  const bearerHeader = req.headers.authorization
+  if (typeof bearerHeader !== 'undefined') {
+    const bearer = bearerHeader.split(' ')
+    const bearerToken = bearer[1]
+    const authData = jwt.verify(bearerToken, process.env.JWT_SECRET_KEY, {
+      algorithms: ['HS256']
+    })
+    req.authData = authData
+    next()
+  } else {
+    res.sendStatus(403)
+  }
+}
+
+app.post('/api/login', (req, res) => {
+  const userPassword = req.body.password
+  const userEmail = req.body.email
+  if ((typeof userEmail || typeof userPassword) !== 'string') {
+    res.status(401).send({ message: 'Invalid email or password' })
+    return
+  }
+  const sqlQuery = 'SELECT id, email, password FROM Users where Users.email = ?'
+  connection.query(sqlQuery, userEmail, (err, matchs) => {
+    if (err) {
+      console.log(err)
+      res.status(500).send({ message: 'Error retrieving user and password' })
+      return
+    }
+    const oneMatchFound = matchs.length === 1
+    if (!oneMatchFound || matchs[0].password !== userPassword) {
+      res.status(401).send({ message: 'Wrong email or password' })
+      return
+    } else {
+      jwt.sign(
+        { sub: matchs[0].id, iss: 'batch-cooker', scopes: ['admin', 'user'] },
+        process.env.JWT_SECRET_KEY,
+        { algorithm: 'HS256', expiresIn: '1h' },
+        (err, token) => {
+          if (err) {
+            console.log(err)
+            res.status(500).send({ message: 'Error signing token' })
+          }
+          res.cookie('access_token', token, { httpOnly: true, sameSite: true })
+          res.status(200).send({ message: 'Login suceeded' })
+        }
+      )
+    }
+  })
+})
+
+app.post('/api/register', (req, res) => {
+  const formData = {
+    email: req.body.email,
+    password: req.body.password,
+    username: req.body.username
+  }
+
+  models.User.create({ ...formData })
+    .then(res.status(201))
+    .catch(err => console.log(err))
+  // const sqlQuery = 'INSERT INTO Users SET ?'
+  // connection.query(sqlQuery, formData, err => {
+  //   if (err) {
+  //     console.log(err)
+  //     res.status(500).send({ message: 'Error registering user' })
+  //   } else {
+  //     res.sendStatus(200)
+  //   }
+  // })
+})
+
+module.exports = app
