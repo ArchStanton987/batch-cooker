56694eb0a62d79f9c1458692e95ca8d63ef18a4a
Pierre Moulin <phv.moulin@gmail.com>
2020-09-29 15:46:35 +0200

    front : manage state of ingredient filters

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -6,6 +6,17 @@ import SearchBox from '../containers/SearchBox'
 import '../sass/pages/_Inventory.scss'
 
 export default function Inventory() {
+  let includeCategories = {
+    spices: true,
+    dairy: true,
+    meat: true,
+    cereal: true,
+    fruits: true,
+    sweet: true,
+    other: true
+  }
+
+  const [activeCategories, setActiveCategories] = useState(includeCategories)
   const [inventory, setInventory] = useState([])
   const [isExpended, setIsExpended] = useState(true)
 
@@ -18,6 +29,15 @@ export default function Inventory() {
     isExpended ? setIsExpended(false) : setIsExpended(true)
   }
 
+  const setActiveCategory = e => {
+    let categoryName = e.target.name
+    activeCategories[categoryName] = !activeCategories[categoryName]
+    let updatedValues = activeCategories.categoryName
+    setActiveCategories(prevState => {
+      return { ...prevState, ...updatedValues }
+    })
+  }
+
   useEffect(() => {
     getInventory()
   }, [])
@@ -26,37 +46,109 @@ export default function Inventory() {
     <>
       <h2>Inventaire</h2>
       <div className="inventory-category h3-container">
-        <div className="inventory-category-circle">
-          <div
-            className={isExpended ? 'inventory-category-arrow' : 'inventory-category-arrow rotated'}
-          ></div>
+        <div onClick={drawer} className="drawer-container">
+          <div className="inventory-category-circle">
+            <div
+              className={
+                isExpended ? 'inventory-category-arrow' : 'inventory-category-arrow rotated'
+              }
+            ></div>
+          </div>
+          <h3>Catégories</h3>
         </div>
-        <h3 onClick={drawer}>Catégories</h3>
         <ul
           className={isExpended ? 'inventory-category-list' : 'inventory-category-list retracted'}
         >
           <li>
-            <button className="inventory-category-name-inactive">
+            <button
+              name="spices"
+              onClick={setActiveCategory}
+              className={
+                activeCategories.spices
+                  ? 'inventory-category-name'
+                  : 'inventory-category-name-inactive'
+              }
+            >
               assaisonnements / condiments
             </button>
           </li>
           <li>
-            <button className="inventory-category-name">produits laitiers</button>
+            <button
+              name="dairy"
+              onClick={setActiveCategory}
+              className={
+                activeCategories.dairy
+                  ? 'inventory-category-name'
+                  : 'inventory-category-name-inactive'
+              }
+            >
+              produits laitiers
+            </button>
           </li>
           <li>
-            <button className="inventory-category-name">viandes / poissons</button>
+            <button
+              name="meat"
+              onClick={setActiveCategory}
+              className={
+                activeCategories.meat
+                  ? 'inventory-category-name'
+                  : 'inventory-category-name-inactive'
+              }
+            >
+              viandes / poissons
+            </button>
           </li>
           <li>
-            <button className="inventory-category-name">céréales / féculents</button>
+            <button
+              name="cereal"
+              onClick={setActiveCategory}
+              className={
+                activeCategories.cereal
+                  ? 'inventory-category-name'
+                  : 'inventory-category-name-inactive'
+              }
+            >
+              céréales / féculents
+            </button>
           </li>
           <li>
-            <button className="inventory-category-name-inactive">fruits / légumes</button>
+            <button
+              name="fruits"
+              onClick={setActiveCategory}
+              className={
+                activeCategories.fruits
+                  ? 'inventory-category-name'
+                  : 'inventory-category-name-inactive'
+              }
+            >
+              fruits / légumes
+            </button>
           </li>
           <li>
-            <button className="inventory-category-name">sucrés</button>
+            <button
+              name="sweet"
+              onClick={setActiveCategory}
+              className={
+                activeCategories.sweet
+                  ? 'inventory-category-name'
+                  : 'inventory-category-name-inactive'
+              }
+            >
+              sucrés
+            </button>
           </li>
           <li>
-            <button className="inventory-category-name">autres</button>
+            <button
+              name="other"
+              onClick={setActiveCategory}
+              className={
+                activeCategories.other
+                  ? 'inventory-category-name'
+                  : 'inventory-category-name-inactive'
+              }
+            >
+              autres
+            </button>
           </li>
         </ul>
       </div>
