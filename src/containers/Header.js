7159bd19169aac343c517caa36b5c763e8da8453
Pierre Moulin <phv.moulin@gmail.com>
2020-10-05 12:18:59 +0200

    front : remove top searchbox & fix search animation

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,23 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import homeIcon from '../assets/img/home-icon.png'
-import searchIcon from '../assets/img/search.svg'
 import menuIcon from '../assets/img/burger.svg'
 import '../sass/layout/_Header.scss'
 
-export default function Header({ toggleFunction, setMenu, setSearchBox }) {
+export default function Header({ toggleFunction, setMenu }) {
   return (
     <header className="header">
       <Link id="homeIcon-link" to="/">
         <img className="header_icon" src={homeIcon} alt="home" />
       </Link>
       <h1>BatchCooker</h1>
-      <img
-        onClick={() => toggleFunction(setSearchBox)}
-        className="header_icon"
-        src={searchIcon}
-        alt="search"
-      />
       <img
         className="header_icon"
         src={menuIcon}
--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -5,16 +5,15 @@ import Menu from './Menu'
 import '../sass/layout/_Layout.scss'
 
 export default function Layout() {
-  const [isSearchBoxActive, setSearchBox] = useState(false)
   const [isSideMenuActive, setMenu] = useState(false)
 
   const toggleFunction = setHook => setHook(prevState => !prevState)
 
   return (
     <div className="global-layout">
-      <Header toggleFunction={toggleFunction} setMenu={setMenu} setSearchBox={setSearchBox} />
+      <Header toggleFunction={toggleFunction} setMenu={setMenu} />
       <Menu toggleFunction={toggleFunction} setMenu={setMenu} isSideMenuActive={isSideMenuActive} />
-      <Main isSearchBoxActive={isSearchBoxActive} />
+      <Main />
     </div>
   )
 }
