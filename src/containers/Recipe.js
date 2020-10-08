496db2baa0bb449900a4870c1749b2b4d8c063e6
Pierre Moulin <phv.moulin@gmail.com>
2020-10-08 17:34:49 +0200

    front : default recipe img & style for recipes

--- src/containers/Recipe.js
+++ src/containers/Recipe.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
+import defaultImg from '../assets/images/defaultRecipe.jpg'
 import '../sass/components/_Recipe.scss'
 
 export default function Recipe({ recipe }) {
   return (
     <>
-      <li className="recipe-list-item">
-        <p>{recipe.name}</p>
+      <li className="recipe-card">
+        <h5>{recipe.name}</h5>
+        <img className="recipe_image" src={recipe.image || defaultImg} alt={recipe.name} />
       </li>
     </>
   )
