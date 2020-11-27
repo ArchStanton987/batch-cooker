2a4fc36d3e324edd4ddd3d84fdb818745860c08b
Pierre Moulin <phv.moulin@gmail.com>
2020-11-27 15:18:23 +0100

    update port

--- server/server.js
+++ server/server.js
@@ -2,9 +2,11 @@ const path = require('path')
 require('dotenv').config({ path: path.resolve('../', '.env') })
 const app = require('./app')
 
-app.listen(process.env.SERVER_PORT, err => {
+const port = process.env.SERVER_PORT || 8000
+
+app.listen(port, err => {
   if (err) {
     throw new Error('Something bad happened...')
   }
-  console.log(`Server is listening on ${process.env.SERVER_PORT}`)
+  console.log(`Server is listening on ${port}`)
 })
