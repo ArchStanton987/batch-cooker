76d75e0ee12bc9e8c235a9c188693f8e0cff3206
Pierre Moulin <phv.moulin@gmail.com>
2020-11-30 13:14:36 +0100

    remove cluster

--- server/app.js
+++ server/app.js
@@ -1,7 +1,5 @@
 const express = require('express')
 const path = require('path')
-const cluster = require('cluster')
-const numCPUs = require('os').cpus().length
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
@@ -11,49 +9,34 @@ const inventory = require('./routes/inventory')
 const shoppinglist = require('./routes/shoppinglist')
 const recipes = require('./routes/recipes')
 
-const isDev = process.env.NODE_ENV !== 'production'
-
 const app = express()
+app.use(express.static(path.resolve(__dirname, '../build')))
 
-if (!isDev && cluster.isMaster) {
-  console.error(`Node cluster master ${process.pid} is running`)
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork()
-  }
-  cluster.on('exit', (worker, code, signal) => {
-    console.error(
-      `Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`
-    )
+app.use(
+  cors({
+    credentials: true,
+    origin: [
+      'http://localhost:3000',
+      'http://192.168.1.27:3000',
+      'http://localhost:8000',
+      'http://192.168.1.27:8000',
+      'https://batch-cooker.herokuapp.com/'
+    ],
+    optionsSuccessStatus: 200
   })
-} else {
-  app.use(express.static(path.resolve(__dirname, '../build')))
+)
 
-  app.use(
-    cors({
-      credentials: true,
-      origin: [
-        'http://localhost:3000',
-        'http://192.168.1.27:3000',
-        'http://localhost:8000',
-        'http://192.168.1.27:8000',
-        'https://batch-cooker.herokuapp.com/'
-      ],
-      optionsSuccessStatus: 200
-    })
-  )
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use('/api/users', users)
+app.use('/api/account', account)
+app.use('/api/inventory', inventory)
+app.use('/api/recipes', recipes)
+app.use('/api/shoppinglist', shoppinglist)
 
-  app.use(bodyParser.json())
-  app.use(bodyParser.urlencoded({ extended: true }))
-  app.use(cookieParser())
-  app.use('/api/users', users)
-  app.use('/api/account', account)
-  app.use('/api/inventory', inventory)
-  app.use('/api/recipes', recipes)
-  app.use('/api/shoppinglist', shoppinglist)
-
-  app.get('*', function (req, res) {
-    res.sendFile(path.resolve(__dirname, '../build', 'index.html'))
-  })
-}
+app.get('*', function (req, res) {
+  res.sendFile(path.resolve(__dirname, '../build', 'index.html'))
+})
 
 module.exports = app
