ba950897e45a8904817106369e53441d24fcc4aa
Pierre Moulin <phv.moulin@gmail.com>
2020-11-30 12:57:17 +0100

    update server app.js

--- server/app.js
+++ server/app.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const path = require('path')
+const cluster = require('cluster')
+const numCPUs = require('os').cpus().length
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
@@ -8,23 +11,49 @@ const inventory = require('./routes/inventory')
 const shoppinglist = require('./routes/shoppinglist')
 const recipes = require('./routes/recipes')
 
-const app = express()
+const isDev = process.env.NODE_ENV !== 'production'
 
-app.use(
-  cors({
-    credentials: true,
-    origin: ['http://localhost:3000', 'http://192.168.1.27:3000', 'https://batch-cooker.herokuapp.com/'],
-    optionsSuccessStatus: 200
+if (!isDev && cluster.isMaster) {
+  console.error(`Node cluster master ${process.pid} is running`)
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork()
+  }
+  cluster.on('exit', (worker, code, signal) => {
+    console.error(
+      `Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`
+    )
   })
-)
+} else {
+  const app = express()
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cookieParser())
-app.use('/api/users', users)
-app.use('/api/account', account)
-app.use('/api/inventory', inventory)
-app.use('/api/recipes', recipes)
-app.use('/api/shoppinglist', shoppinglist)
+  app.use(express.static(path.resolve(__dirname, '../build')))
 
-module.exports = app
+  app.use(
+    cors({
+      credentials: true,
+      origin: [
+        'http://localhost:3000',
+        'http://192.168.1.27:3000',
+        'http://localhost:8000',
+        'http://192.168.1.27:8000',
+        'https://batch-cooker.herokuapp.com/*'
+      ],
+      optionsSuccessStatus: 200
+    })
+  )
+
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({ extended: true }))
+  app.use(cookieParser())
+  app.use('/api/users', users)
+  app.use('/api/account', account)
+  app.use('/api/inventory', inventory)
+  app.use('/api/recipes', recipes)
+  app.use('/api/shoppinglist', shoppinglist)
+
+  app.get('*', function (req, res) {
+    res.sendFile(path.resolve(__dirname, '../build', 'index.html'))
+  })
+
+  module.exports = app
+}
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path')
 require('dotenv').config({ path: path.resolve('../', '.env') })
 const app = require('./app')
 
-const port = process.env.SERVER_PORT || 8000
+const port = process.env.PORT || 8000
 
 app.listen(port, err => {
   if (err) {
